89a2ab472f306e79692883b9417cf875639d38ac
Mia Huebscher <m.huebscher@att.net>
2025-09-05 23:29:36 -0700

    Update restuarants-map.tsx

--- src/Map/restuarants-map.tsx
+++ src/Map/restuarants-map.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MapContainer, TileLayer, Marker, Tooltip, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import './Map.css';
 import L from 'leaflet';
