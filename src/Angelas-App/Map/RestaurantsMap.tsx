47c3afa8d95a54f406fd88b2ee9afd9a1a7aaff3
Mia Huebscher <m.huebscher@att.net>
2025-09-27 13:38:47 -0700

    Update RestaurantsMap.tsx

--- src/Angelas-App/Map/RestaurantsMap.tsx
+++ src/Angelas-App/Map/RestaurantsMap.tsx
@@ -287,8 +287,8 @@ export default function RestaurantsMap() {
           <TileLayer
             url={`https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=KA8DXtaZOGzGWg60NAIo`}
             attribution='&copy; <a href="https://www.maptiler.com/">MapTiler</a> &copy; OpenStreetMap contributors'
-            crossOrigin=""
-            referrerPolicy="strict-origin"
+            crossOrigin="anonymous"
+            referrerPolicy="no-referrer-when-downgrade"
           />
           {mapReady && filteredRestaurants.map((loc) => (
             <Marker key={loc._id} position={loc.coords as [number, number]} icon={customIcon}>
